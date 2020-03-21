cacd973028c027940dfd4b9719c089b7150ae25e
Dino Numic <dino.emp@gmail.com>
2020-03-22 00:23:04 +0100

    Added shameless self plug footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="text-center">
+        <p class="mb-1">
+            <strong>
+                Developed by <a class="text-decoration-none" target="_blank" href="https://github.com/Nodimon">Dino Numic</a>
+            </strong>
+        </p>
+        <p>
+            <strong>
+                <a target="_blank" class="text-decoration-none" href="https://codinary.dev/">@Codinary</a>
+            </strong>
+        </p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Footer"
+    }
+</script>
--- src/router/index.js
+++ src/router/index.js
@@ -6,29 +6,15 @@ Vue.use(VueRouter)
 const routes = [
     {
         path: '/',
-        component: () => import('../App'),
-        children: [
-            {
-                path: '/',
-                component: () => import('../views/World'),
-                name: 'world-view'
-            },
-            {
-                path: '/country/:country',
-                component: () => import('../views/Country'),
-                name: 'country-view',
-                props: true
-            }
-        ]
+        component: () => import('../views/World'),
+        name: 'world-view'
     },
-  // {
-  //   path: '/about',
-  //   name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
+    {
+        path: '/country/:country',
+        component: () => import('../views/Country'),
+        name: 'country-view',
+        props: true
+    }
 ]
 
 const router = new VueRouter({
